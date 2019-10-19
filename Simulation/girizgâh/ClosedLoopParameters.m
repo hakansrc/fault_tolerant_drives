08beb461660f9d6ef607286560a0e51503d165ff
hakan saraç <hakansrc@msn.com>
2019-10-19 10:43:40 +0300

    Açık döngü model oluşturuldu, kapalı döngüye girizgah yapıyoruz

---
+++ Simulation/girizgâh/ClosedLoopParameters.m
@@ -0,0 +1,13 @@
+clc
+clear
+Sampling_time = 1e-6;
+Tend = 5;
+DCLINK_Cap = 100e-6;
+Rin = 1e-6;
+Pout = 4000;
+Vdesired = 540;
+Vin = Vdesired;%Vdesired+Pout/Vdesired*Rin;
+sw_frequency = 20e3;
+interleaving_angle_1 = 0;
+ref_frequency = 2*pi*50;
+ma = 0.9; %asd

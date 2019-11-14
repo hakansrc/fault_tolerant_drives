d2a741242e301bfd977878b2b015cfeb5146811a
hakan saraç <hakansrc@msn.com>
2019-11-14 21:27:17 +0300

    motor blokunun içini ben doldursam nasıl olur? sorusu üzerine kafa yoruldu ve ilk taslak model çıkartıldı.

---
+++ Simulation/analitik_denemeler/AnalyticalParameters.m
@@ -0,0 +1,18 @@
+clc
+clear
+Sampling_time = 1e-6;
+Tend = 5;
+DCLINK_Cap = 100e-6;
+Rin = 1e-6;
+Pout = 4000;
+Vdesired = 540;
+Vin = Vdesired;%Vdesired+Pout/Vdesired*Rin;
+sw_frequency = 20e3;
+interleaving_angle_1 = 0;
+ref_frequency = 2*pi*50;
+Tload = 15;
+ma = 0.9; 
+%% machine parameters
+M1_PhaseA_Ls = 3.3e-3;
+M1_PhaseB_Ls = 3.3e-3;
+M1_PhaseC_Ls = 3.3e-3;

fa66ff06cc2fc79ab00d8455a755199e8680f97c
hakan saraç <hakansrc@msn.com>
2019-11-30 18:06:58 +0300

    canlı olarak waveform izlemek veya kaydetmek birden fazla waveform için hayal gibi görünüyor şimdilik. 40khz ile gönderilen bir 4byte değişken için yaklaşık 150-160kB/sec data rate gerekiyor, seri kanal en fazla 90kB/sec data rate destekliyor. Daha hızlı olduğu düşünülen aşağıdaki linkteki usb to ttl cihazı sipariş edildi. onunle deneriz geldiğinde, onunla teorik olarak 300kB/sec değerlerine çıkabileceğiz(DSP'nin maks limiti)https://urun.n11.com/diger/usb-to-ttl-uart-pl2303hx-modul-P392680336.
    Gerektiğinde DSP'deki ram üzerine 48kb'a kadar veri kaydedilip parça parça aktarma yapılabilir, ilerki aşamalar için düşünülebilir. Bunu yapmak oldukça trivial.
    
    Ekteki script dosyalarında float değişkenlerinin gönderilip kaydedilip daha sonra kullanımı için örneklerdir. Örnek bir mat dosyası da konulmuştur.
    
    Tagleme konusunda kafam karışık, çünkü bandwidth çok dar. Belki tek bir data maksimum ratede taglenmeden gönderilip diğer mean (veya rms) değerler taglenip gönderilebilir. bir araya geldiğimizde konuşuruz.
    
    Bilginize
    
    @ozank @mesutto

--- Garbage/serial/SaveTheSerialData.m
+++ Garbage/serial/SaveTheSerialData.m
@@ -4,42 +4,44 @@ fclose(instrfind) % call this command for stopping callback function
 %% IMPORTANT DEFINITIONS
 global CallbackFunctionByteNumber
 global TheSaveAmount
-BaudRateValue = 230400;                 %baudrate of the serial channel can be set from here
+BaudRateValue = 460800*2;               %921600 is the maximum baudrate value for now
 TimeoutValue = 6;                       %Allowed time in seconds to complete read and write operations, returned as a numeric value. 
 CallbackFunctionByteNumber=512;         %the callback function is called when this amount of bytes are read from the channel
-TheSaveAmount = 4*1024;               %the callback function save array size in bytes  
+TheSaveAmount = 20*1024;                %the callback function save array size in bytes  
 
 %% IMPORTANT VARIABLES
 % global variables (used for getting variables from the callback function)
-global SerialReadArray          %the read values are put here first
-global SerialReadOrdered        %the read values at SerialReadArray are aligned inside this array
-global SerialReadConverted      %the order values at SerialReadConverted converted properly and saved inside this array
-global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it
-global NumberOfReceivedBytes    %this is a variable that stores number of bytes read from the serial channel, required for array operations
-global TheSaveArray             %The callback function stores the data inside this large array
- 
-
+global SerialReadArray          %the read values are put here first, in bytes
+global SerialReadOrdered        %the read values at SerialReadArray are aligned inside this array, in bytes
+global SerialReadConverted      %the order values at SerialReadConverted converted properly and saved inside this array, in float
+global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it, 1 or 0 
+global NumberOfReceivedBytes    %this is a variable that stores number of bytes read from the serial channel, required for array operations, 
+global TheSaveArray             %The callback function stores the data inside this large array, the values are converted from this array
 
 %% Main function 
-NumberOfReceivedBytes = 0;
+NumberOfReceivedBytes = 0; %init the number of bytes
+SaveTheArrayFlag = 0;       %init the flag
 SerialChannel = serial('COM4','BaudRate',BaudRateValue,'Parity','none','Timeout',6);
 SerialChannel.BytesAvailableFcnCount = CallbackFunctionByteNumber;
 SerialChannel.BytesAvailableFcnMode = 'byte';
 SerialChannel.BytesAvailableFcn = @SerialReadCallbackFunction;
-% plot(x,y,'ButtonDownFcn',@SerialReadServant)
 fopen(SerialChannel);
-TheSaveArray = zeros(TheSaveAmount,1);  
-SerialReadConverted = zeros(TheSaveAmount/4,1);
+TheSaveArray = zeros(TheSaveAmount,1);          %init the array
+SerialReadConverted = zeros(TheSaveAmount/4,1); %init the array
+mkdir TestData                                  %create directory for saving data
 while(1)
     if(SaveTheArrayFlag==1)
         for i=1:1:(TheSaveAmount/4)
             SerialReadConverted(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArray(4+4*(i-1)),2),dec2hex(TheSaveArray(3+4*(i-1)),2),dec2hex(TheSaveArray(2+4*(i-1)),2),dec2hex(TheSaveArray(1+4*(i-1)),2)))),'single');
         end
-        plot(SerialReadConverted);
-        drawnow;
+%         plot(SerialReadConverted);
+        FileName = sprintf("TestData/TestData%s", datestr(now, 'ddmmyyHHMMSS'));
+        save(FileName);
+%         ylim([0 1])
+%         drawnow;
         SaveTheArrayFlag = 0;
     else
-         pause(0.01);
+         pause(1);%wait for one second between operations
     end
 end
 %% The callback function

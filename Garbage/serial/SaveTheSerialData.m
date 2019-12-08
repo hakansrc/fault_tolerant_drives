dd2e4eca6ae077ac8407bb83310597fd6e65ac3f
hakan saraç <hakansrc@msn.com>
2019-12-08 19:01:37 +0300

    "main.c" tarafına tag ile data göndermek için kısımlar eklendi. Matlab kısmına ilgi tagdeki dataları ayıklamak için kısımlar eklendi. Önümüzdeki hafta için bu kodun ve encoderin kontrol kartına entegrasyonu üzerine çalışılacak

--- Garbage/serial/SaveTheSerialData.m
+++ Garbage/serial/SaveTheSerialData.m
@@ -1,24 +1,31 @@
 clc
 clear
-fclose(instrfind) % call this command for stopping callback function
+% fclose(instrfind) % call this command for stopping callback function
 %% IMPORTANT DEFINITIONS
 global CallbackFunctionByteNumber
 global TheSaveAmount
 BaudRateValue = 460800*2;               %921600 is the maximum baudrate value for now
-TimeoutValue = 6;                       %Allowed time in seconds to complete read and write operations, returned as a numeric value. 
+TimeoutValue = 6;                       %Allowed time in seconds to complete read and write operations, returned as a numeric value.
 CallbackFunctionByteNumber=512;         %the callback function is called when this amount of bytes are read from the channel
-TheSaveAmount = 20*1024;                %the callback function save array size in bytes  
+TheSaveAmount = 100*1024;                %the callback function save array size in bytes
+TagNumber1 = '0001';
+TagNumber2 = '0002';
+TagNumber3 = '0003';
+TagNumber4 = '0004';
+TagNumber5 = '0005';
+
+EnableSaving = 0;                         %set 0 in order to disable saving of workspace
+EnablePlotting = 1;                     %set 0 in order to disable plotting of taken variables
 
 %% IMPORTANT VARIABLES
 % global variables (used for getting variables from the callback function)
 global SerialReadArray          %the read values are put here first, in bytes
 global SerialReadOrdered        %the read values at SerialReadArray are aligned inside this array, in bytes
-global SerialReadConverted      %the order values at SerialReadConverted converted properly and saved inside this array, in float
-global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it, 1 or 0 
-global NumberOfReceivedBytes    %this is a variable that stores number of bytes read from the serial channel, required for array operations, 
-global TheSaveArray             %The callback function stores the data inside this large array, the values are converted from this array
+global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it, 1 or 0
+global NumberOfReceivedBytes    %this is a variable that stores number of bytes read from the serial channel, required for array operations,
+global TheSaveArrayRaw             %The callback function stores the data inside this large array, the values are converted from this array
 
-%% Main function 
+%% Main function
 NumberOfReceivedBytes = 0; %init the number of bytes
 SaveTheArrayFlag = 0;       %init the flag
 SerialChannel = serial('COM4','BaudRate',BaudRateValue,'Parity','none','Timeout',6);
@@ -26,45 +33,173 @@ SerialChannel.BytesAvailableFcnCount = CallbackFunctionByteNumber;
 SerialChannel.BytesAvailableFcnMode = 'byte';
 SerialChannel.BytesAvailableFcn = @SerialReadCallbackFunction;
 fopen(SerialChannel);
-TheSaveArray = zeros(TheSaveAmount,1);          %init the array
-SerialReadConverted = zeros(TheSaveAmount/4,1); %init the array
-mkdir TestData                                  %create directory for saving data
+TheSaveArrayRaw = zeros(TheSaveAmount,1);          %init the array
+if (EnableSaving==1)
+    mkdir TestData                                  %create directory for saving data
+end
+MainSignalOffset = 0;
+IsMainSignalOffsetProper = 0;
 while(1)
+    
     if(SaveTheArrayFlag==1)
-        for i=1:1:(TheSaveAmount/4)
-            SerialReadConverted(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArray(4+4*(i-1)),2),dec2hex(TheSaveArray(3+4*(i-1)),2),dec2hex(TheSaveArray(2+4*(i-1)),2),dec2hex(TheSaveArray(1+4*(i-1)),2)))),'single');
+        IsMainSignalOffsetProper = 0;
+        TheSaveArrayScreened = TheSaveArrayRaw;
+        %% Tag 1 conversion and elimination
+        Tag_1_Indices = strfind(TheSaveArrayScreened',TagNumber1);
+        if(isempty(Tag_1_Indices)==0)
+            if(IsMainSignalOffsetProper==0)
+                MainSignalOffset = mod((Tag_1_Indices(1)-1),4);
+                IsMainSignalOffsetProper = 1;
+            end
+            Tag_1_Value = zeros(1,numel(Tag_1_Indices));
+            for i=1:numel(Tag_1_Indices)
+                Tag_1_Indices(i) = Tag_1_Indices(i)-(i-1)*8;
+                if (Tag_1_Indices(i)+8)<=numel(TheSaveArrayScreened)
+                    Tag_1_Value(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArrayScreened(7+Tag_1_Indices(i)),2),dec2hex(TheSaveArrayScreened(6+Tag_1_Indices(i)),2),dec2hex(TheSaveArrayScreened(5+Tag_1_Indices(i)),2),dec2hex(TheSaveArrayScreened(4+Tag_1_Indices(i)),2)))),'single')
+                    for j = 1:8
+                        TheSaveArrayScreened(Tag_1_Indices(i)) = [];
+                    end
+                else
+                    for j = 1:(numel(TheSaveArrayScreened)-Tag_1_Indices(i))
+                        TheSaveArrayScreened(Tag_1_Indices(i)) = [];
+                    end
+                end
+            end
         end
-%         plot(SerialReadConverted);
-        FileName = sprintf("TestData/TestData%s", datestr(now, 'ddmmyyHHMMSS'));
-        save(FileName);
-%         ylim([0 1])
-%         drawnow;
+        %% Tag 2 conversion and elimination
+        Tag_2_Indices = strfind(TheSaveArrayScreened',TagNumber2);
+        if(isempty(Tag_2_Indices)==0)
+            if(IsMainSignalOffsetProper==0)
+                MainSignalOffset = mod((Tag_2_Indices(1)-1),4);
+                IsMainSignalOffsetProper = 1;
+            end            
+            Tag_2_Value = zeros(1,numel(Tag_2_Indices));
+            for i=1:numel(Tag_2_Indices)
+                Tag_2_Indices(i) = Tag_2_Indices(i)-(i-1)*8;
+                if (Tag_2_Indices(i)+8)<=numel(TheSaveArrayScreened)
+                    Tag_2_Value(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArrayScreened(7+Tag_2_Indices(i)),2),dec2hex(TheSaveArrayScreened(6+Tag_2_Indices(i)),2),dec2hex(TheSaveArrayScreened(5+Tag_2_Indices(i)),2),dec2hex(TheSaveArrayScreened(4+Tag_2_Indices(i)),2)))),'single')
+                    for j = 1:8
+                        TheSaveArrayScreened(Tag_2_Indices(i)) = [];
+                    end
+                else
+                    for j = 1:(numel(TheSaveArrayScreened)-Tag_2_Indices(i))
+                        TheSaveArrayScreened(Tag_2_Indices(i)) = [];
+                    end
+                end
+            end
+        end
+        %% Tag 3 conversion and elimination
+        Tag_3_Indices = strfind(TheSaveArrayScreened',TagNumber3);
+        if(isempty(Tag_3_Indices)==0)
+            if(IsMainSignalOffsetProper==0)
+                MainSignalOffset = mod((Tag_3_Indices(1)-1),4);
+                IsMainSignalOffsetProper = 1;
+            end              
+            Tag_3_Value = zeros(1,numel(Tag_3_Indices));
+            for i=1:numel(Tag_3_Indices)
+                Tag_3_Indices(i) = Tag_3_Indices(i)-(i-1)*8;
+                if (Tag_3_Indices(i)+8)<=numel(TheSaveArrayScreened)
+                    Tag_3_Value(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArrayScreened(7+Tag_3_Indices(i)),2),dec2hex(TheSaveArrayScreened(6+Tag_3_Indices(i)),2),dec2hex(TheSaveArrayScreened(5+Tag_3_Indices(i)),2),dec2hex(TheSaveArrayScreened(4+Tag_3_Indices(i)),2)))),'single')
+                    for j = 1:8
+                        TheSaveArrayScreened(Tag_3_Indices(i)) = [];
+                    end
+                else
+                    for j = 1:(numel(TheSaveArrayScreened)-Tag_3_Indices(i))
+                        TheSaveArrayScreened(Tag_3_Indices(i)) = [];
+                    end
+                end
+            end
+        end      
+        %% Tag 4 conversion and elimination
+        Tag_4_Indices = strfind(TheSaveArrayScreened',TagNumber4);
+        if(isempty(Tag_4_Indices)==0)
+            if(IsMainSignalOffsetProper==0)
+                MainSignalOffset = mod((Tag_4_Indices(1)-1),4);
+                IsMainSignalOffsetProper = 1;
+            end              
+            Tag_4_Value = zeros(1,numel(Tag_4_Indices));
+            for i=1:numel(Tag_4_Indices)
+                Tag_4_Indices(i) = Tag_4_Indices(i)-(i-1)*8;
+                if (Tag_4_Indices(i)+8)<=numel(TheSaveArrayScreened)
+                    Tag_4_Value(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArrayScreened(7+Tag_4_Indices(i)),2),dec2hex(TheSaveArrayScreened(6+Tag_4_Indices(i)),2),dec2hex(TheSaveArrayScreened(5+Tag_4_Indices(i)),2),dec2hex(TheSaveArrayScreened(4+Tag_4_Indices(i)),2)))),'single')
+                    for j = 1:8
+                        TheSaveArrayScreened(Tag_4_Indices(i)) = [];
+                    end
+                else
+                    for j = 1:(numel(TheSaveArrayScreened)-Tag_4_Indices(i))
+                        TheSaveArrayScreened(Tag_4_Indices(i)) = [];
+                    end
+                end
+            end
+        end  
+        %% Tag 5 conversion and elimination
+        Tag_5_Indices = strfind(TheSaveArrayScreened',TagNumber5);
+        if(isempty(Tag_5_Indices)==0)
+            if(IsMainSignalOffsetProper==0)
+                MainSignalOffset = mod((Tag_5_Indices(1)-1),4);
+                IsMainSignalOffsetProper = 1;
+            end              
+            Tag_5_Value = zeros(1,numel(Tag_5_Indices));
+            for i=1:numel(Tag_5_Indices)
+                Tag_5_Indices(i) = Tag_5_Indices(i)-(i-1)*8;
+                if (Tag_5_Indices(i)+8)<=numel(TheSaveArrayScreened)
+                    Tag_5_Value(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArrayScreened(7+Tag_5_Indices(i)),2),dec2hex(TheSaveArrayScreened(6+Tag_5_Indices(i)),2),dec2hex(TheSaveArrayScreened(5+Tag_5_Indices(i)),2),dec2hex(TheSaveArrayScreened(4+Tag_5_Indices(i)),2)))),'single')
+                    for j = 1:8
+                        TheSaveArrayScreened(Tag_5_Indices(i)) = [];
+                    end
+                else
+                    for j = 1:(numel(TheSaveArrayScreened)-Tag_5_Indices(i))
+                        TheSaveArrayScreened(Tag_5_Indices(i)) = [];
+                    end
+                end
+            end
+        end          
+        %% Raw data conversion
+        MainSignalConverted = zeros(TheSaveAmount/4-MainSignalOffset-8*numel(Tag_1_Indices)-8*numel(Tag_2_Indices)-8*numel(Tag_3_Indices)-8*numel(Tag_4_Indices)-8*numel(Tag_5_Indices),1); %init the array
+        for i=1:1:(TheSaveAmount/4-MainSignalOffset-8*numel(Tag_1_Indices)-8*numel(Tag_2_Indices)-8*numel(Tag_3_Indices)-8*numel(Tag_4_Indices)-8*numel(Tag_5_Indices))
+            MainSignalConverted(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArrayScreened(4+4*(i-1)+MainSignalOffset),2),dec2hex(TheSaveArrayScreened(3+4*(i-1)+MainSignalOffset),2),dec2hex(TheSaveArrayScreened(2+4*(i-1)+MainSignalOffset),2),dec2hex(TheSaveArrayScreened(1+4*(i-1)+MainSignalOffset),2)))),'single');
+        end
+              
+        if (EnablePlotting==1)
+            plot(MainSignalConverted);
+            ylim([0 1])
+            drawnow;
+        end
+        if (EnableSaving==1)
+            FileName = sprintf("TestData/TestData%s", datestr(now, 'ddmmyyHHMMSS'));
+            save(FileName);
+        end
         SaveTheArrayFlag = 0;
     else
-         pause(1);%wait for one second between operations
+        if(EnableSaving==1)
+            pause(1);%wait for one second between operations
+        else
+            pause(0.01);    %if your computer's fan blows high, increase this value
+        end
+        
     end
 end
 %% The callback function
 function [] =  SerialReadCallbackFunction(TheSerialChannel,TheEvent)
-    global NumberOfReceivedBytes
-    global CallbackFunctionByteNumber
-    global SerialReadArray          %the read values are put here first
-    global TheSaveArray             %The callback function stores the data inside this large array, initialized inside the main function
-    global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it
-    global TheSaveAmount
+global NumberOfReceivedBytes
+global CallbackFunctionByteNumber
+global SerialReadArray          %the read values are put here first
+global TheSaveArrayRaw             %The callback function stores the data inside this large array, initialized inside the main function
+global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it
+global TheSaveAmount
 
-    if(SaveTheArrayFlag==0)
-        SerialReadArray = fread(TheSerialChannel,TheSerialChannel.BytesAvailable);
-        TheSaveArray((NumberOfReceivedBytes+1):(NumberOfReceivedBytes+CallbackFunctionByteNumber),1)=SerialReadArray;
-        NumberOfReceivedBytes = NumberOfReceivedBytes+512;
-    else
-        GoToGarbage = fread(TheSerialChannel,TheSerialChannel.BytesAvailable); %just read and pass, dont store , otherwise the callback does not work
-    end
+if(SaveTheArrayFlag==0)
+    SerialReadArray = fread(TheSerialChannel,TheSerialChannel.BytesAvailable);
+    TheSaveArrayRaw((NumberOfReceivedBytes+1):(NumberOfReceivedBytes+CallbackFunctionByteNumber),1)=SerialReadArray;
+    NumberOfReceivedBytes = NumberOfReceivedBytes+512;
+else
+    GoToGarbage = fread(TheSerialChannel,TheSerialChannel.BytesAvailable); %just read and pass, dont store , otherwise the callback does not work
+end
+
+if(NumberOfReceivedBytes>=TheSaveAmount)
     
-    if(NumberOfReceivedBytes>=TheSaveAmount)
-        
-        NumberOfReceivedBytes = 0;
-        SaveTheArrayFlag = 1;
-    end 
+    NumberOfReceivedBytes = 0;
+    SaveTheArrayFlag = 1;
+end
 
 end
--- Garbage/serial/main.c
+++ Garbage/serial/main.c
@@ -10,16 +10,23 @@
 
 #define SCIBRXBUFFERSIZE 256
 #define PI              3.141592654
-#define SWFREQ 40000           // Hz
+#define SWFREQ 20000           // Hz
+
+#define TAGNUMBER1  "0001"
+#define TAGNUMBER2  "0002"
+#define TAGNUMBER3  "0003"
+#define TAGNUMBER4  "0004"
+#define TAGNUMBER5  "0005"
+
 
 __interrupt void cpu_timer0_isr(void);
 __interrupt void cpu_timer1_isr(void);
 __interrupt void cpu_timer2_isr(void);
 __interrupt void SCIB_RX_FIFO_isr(void);
 void Gpio_Select1();
-void SciSendFloat(float *fVariableToSend);
-void SciSendInteger(long int *iVariableToSend);
-void SciSendUnsignedInteger(unsigned long int *uiVariableToSend);
+int SciSendFloatNoTag(float *fVariableToSend,volatile struct SCI_REGS* SerialChannelPtr);
+int SciSendFloatTagged(float *fVariableToSend,volatile struct SCI_REGS* SerialChannelPtr,unsigned int tagnumber);
+int SciSendUnsignedInteger(unsigned long int *uiVariableToSend,volatile struct SCI_REGS* SerialChannelPtr);
 
 
 LibSciSettings SciSettings;
@@ -31,10 +38,14 @@ float epwm_duty = 0.5;
 float epwm_duty_max = 0;
 
 
-unsigned long int delaytime = 1000000;
+unsigned long int delaytime = 100000;
 float number1= 50.2;
 float number2= -53.4;
 float thescalar = 0.5;
+float thescalar2 = 0.51;
+float thescalar3 = 0.52;
+float thescalar4 = 0.53;
+float thescalar5 = 0.54;
 
 long int intnumber1 = 55;   //or make it int32
 long int intnumber2 = -52;  //or make it int32
@@ -42,6 +53,8 @@ long int intnumber3 = 0;
 unsigned long int uintnumber1 = 1;
 unsigned long int uintnumber2 = 1;
 unsigned long int uintnumber3 = 4294967295;
+double switching_freq = SWFREQ;
+double fund_freq = 50;
 
 
 char carrynumber1[4];
@@ -49,6 +62,8 @@ char carrynumber2[4];
 char *ptrnumber1;
 unsigned int sendthroughsciflag = 0;
 char cSendUnsignedInteger[4];
+Uint32 uliNumberToSend = 0;
+unsigned long long int numberofnegatives = 0;
 int main(void)
 {
     /**/
@@ -69,9 +84,9 @@ int main(void)
     CpuSysRegs.PCLKCR0.bit.GTBCLKSYNC =0;
     EDIS;
     InitCpuTimers();   // For this example, only initialize the Cpu Timers
-    ConfigCpuTimer(&CpuTimer0, 100, 2000000); //2 miliseconds
-    ConfigCpuTimer(&CpuTimer1, 100, 1000000/SWFREQ); //1 seconds
-    ConfigCpuTimer(&CpuTimer2, 100, 1000000); //2 seconds
+    ConfigCpuTimer(&CpuTimer0, 200, 100000); //2 miliseconds
+    ConfigCpuTimer(&CpuTimer1, 200, 1000000/switching_freq); //1 seconds
+    ConfigCpuTimer(&CpuTimer2, 200, 1000000); //2 seconds
 
     //CpuTimer0Regs.PRD.all = 0xFFFFFFFF;
     CpuTimer0Regs.TCR.all = 0x4000; // Use write-only instruction to set TSS bit = 0
@@ -94,32 +109,33 @@ int main(void)
     Gpio_Select1();
 
     EALLOW;
-    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 1;
-    CpuSysRegs.PCLKCR0.bit.GTBCLKSYNC =1;
-
+    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC    = 1;
+    CpuSysRegs.PCLKCR0.bit.GTBCLKSYNC   = 1;
     EDIS;
     IER |= M_INT1;
     IER |= M_INT3;
     IER |= M_INT13;
     IER |= M_INT14;
 
-    SciSettings.BaudRate = BAUD230400;  /*baud 115200*/
+    SciSettings.BaudRate = BAUD921600;  /*baud 115200*/
     SciSettings.CharacterBits = 7;      /*8 character bits*/
     SciSettings.ParityEnable = 0;       /*parity disabled*/
     SciSettings.ParityType = 0;         /*odd parity*/
     SciSettings.StopBit = 0;            /*1 stop bit*/
     InitializeSci_Lib(&ScibRegs,&SciSettings,&SCIB_RX_FIFO_isr);
-    /*
+    //ScibRegs.SCILBAUD.bit.BAUD = 6;
+
     PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+    /*
     PieCtrlRegs.PIEIER3.bit.INTx1 = 1;
     PieCtrlRegs.PIEIER3.bit.INTx2 = 1;
     PieCtrlRegs.PIEIER3.bit.INTx3 = 1;
     */
     EINT;  // Enable Global interrupt INTM
     ERTM;  // Enable Global realtime interrupt DBGM
-    char VariableToSend[22];
-    sprintf(VariableToSend,"merhabalar ben hakan0\n");
-    VariableToSend[10] = '0';
+    //char VariableToSend[22];
+    //sprintf(VariableToSend,"merhabalar ben hakan0\n");
+    //VariableToSend[10] = '0';
     //memcpy((float *)carrynumber1,&number1,4);
     //memcpy(carrynumber2,&number2,4);
     //sprintf(carrynumber1,"%x",number1);
@@ -140,96 +156,48 @@ int main(void)
 #endif
     while(1)
     {
-#if 0
-        ScibUartSend(VariableToSend, 22);
-        if(VariableToSend[20]=='9')
-        {
-            VariableToSend[20] = '0';
-        }
-        else
-        {
-            VariableToSend[20]++;
-        }
-        if(VariableToSend[10]=='9')
-        {
-            VariableToSend[10] = '0';
-        }
-        else
-        {
-            VariableToSend[10]++;
-        }
-#endif
-        //ScibUartSend(carrynumber1, 4);
-        /*
-        ScibUartSend(ptrnumber1, 2);
-        ScibUartSend(ptrnumber1+1, 2);
-        ScibUartSend(ptrnumber1+2, 2);
-        ScibUartSend(ptrnumber1+3, 2);
-        */
-        /*
-        ScibUartSend(carrynumber1, 4);
-        ScibUartSend(carrynumber2, 4);
-        */
 
-        //SciSendFloat(&number1);
-        //SciSendFloat(&number2);
-        //ScibUartSend((char *)&carrynumber1, 4);
-        DELAY_US(delaytime);
-        /*
-        SciSendInteger(&intnumber1);
-        SciSendInteger(&intnumber2);
-        */
-        uintnumber1 = 1073741823*thescalar;
-        /*
-        SciSendUnsignedInteger(&uintnumber1);
-        SciSendUnsignedInteger(&uintnumber2);
-        */
-        if(sendthroughsciflag==1)
-        {
+        DELAY_US(100000);
 
-        }
+        //thescalar += 0.01;
     }
 }
-
-
-
 
 __interrupt void cpu_timer0_isr(void)
 {
     CpuTimer0.InterruptCount++;
+    SciSendFloatTagged(&thescalar,&ScibRegs,1);
+    SciSendFloatTagged(&thescalar2,&ScibRegs,2);
+    SciSendFloatTagged(&thescalar3,&ScibRegs,3);
+    SciSendFloatTagged(&thescalar4,&ScibRegs,4);
+    SciSendFloatTagged(&thescalar5,&ScibRegs,5);
+    thescalar+= 1;
+    thescalar2+= 1;
+    thescalar3+= 1;
+    thescalar4+= 1;
+    thescalar5+= 1;
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+
 }
 
 __interrupt void cpu_timer1_isr(void)
 {
-    GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;   //blue
-    GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;   //blue
+    //GpioDataRegs.GPATOGGLE.bit.GPIO31 = 1;   //blue
+    //GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;   //blue
     CpuTimer1.InterruptCount++;
-    epwm_duty = (0.5*sin(2*PI * 50 * spwm_counter / SWFREQ - 2 * PI / 3) + 1) / 2;
+    epwm_duty = (0.5*sin(2.0*PI * fund_freq * spwm_counter / switching_freq - 2.0* PI / 3.0) + 1) / 2;
+    if (spwm_counter > ((switching_freq / fund_freq) - 1))
+    {
+        spwm_counter = 0;
+    }
     if(epwm_duty>epwm_duty_max)
     {
         epwm_duty_max = epwm_duty;
     }
     spwm_counter += 1;
-    //epwm_duty = 0.012;
-    //SciSendFloat(&epwm_duty);
-    uintnumber1 = epwm_duty*4294967295;
-    SciSendUnsignedInteger(&uintnumber1);
-#if 0
-    if(uintnumber1>uintnumber2)
-    {
-        uintnumber2 = uintnumber1;
-    }
-    if(uintnumber3>uintnumber1)
-    {
-        uintnumber3 = uintnumber1;
-    }
-    if (spwm_counter > ((SWFREQ / 50) - 1))
-    {
-        spwm_counter = 0;
-        sendthroughsciflag = 1;
-    }
-#endif
+    SciSendFloatNoTag(&epwm_duty,&ScibRegs);
+    //SciSendUnsignedInteger(&uliNumberToSend,&ScibRegs);
+
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
 }
 
@@ -310,31 +278,121 @@ __interrupt void SCIB_RX_FIFO_isr(void)
 }
 
 
-void SciSendFloat(float *fVariableToSend)
+
+int SciSendFloatNoTag(float *fVariableToSend,volatile struct SCI_REGS* SerialChannelPtr)
 {
     char cPartitionTheNumber[4];
     cPartitionTheNumber[3] = __byte((int *)fVariableToSend,3);
     cPartitionTheNumber[2] = __byte((int *)fVariableToSend,2);
     cPartitionTheNumber[1] = __byte((int *)fVariableToSend,1);
     cPartitionTheNumber[0] = __byte((int *)fVariableToSend,0);
-    ScibUartSend(cPartitionTheNumber, 4);
+#ifdef SCIA_LIB
+    if(SerialChannelPtr==&SciaRegs)
+    {
+        return SciaUartSend(cPartitionTheNumber, 4);
+    }
+#endif
+#ifdef SCIB_LIB
+    if(SerialChannelPtr==&ScibRegs)
+    {
+        return ScibUartSend(cPartitionTheNumber, 4);
+    }
+#endif
+#ifdef SCIC_LIB
+    if(SerialChannelPtr==&ScicRegs)
+    {
+        return ScicUartSend(cPartitionTheNumber, 4);
+    }
+#endif
+#ifdef SCID_LIB
+    if(SerialChannelPtr==&ScidRegs)
+    {
+        return ScidUartSend(cPartitionTheNumber, 4);
+    }
+#endif
+
+    return -1;
+}int SciSendFloatTagged(float *fVariableToSend,volatile struct SCI_REGS* SerialChannelPtr,unsigned int tagnumber)
+{
+    char cPartitionTheNumber[8];
+    cPartitionTheNumber[7] = __byte((int *)fVariableToSend,3);
+    cPartitionTheNumber[6] = __byte((int *)fVariableToSend,2);
+    cPartitionTheNumber[5] = __byte((int *)fVariableToSend,1);
+    cPartitionTheNumber[4] = __byte((int *)fVariableToSend,0);
+    switch(tagnumber)
+    {
+        case 1:
+        {
+            memcpy(cPartitionTheNumber,TAGNUMBER1,4);
+            break;
+        }
+        case 2:
+        {
+            memcpy(cPartitionTheNumber,TAGNUMBER2,4);
+            break;
+        }
+        case 3:
+        {
+            memcpy(cPartitionTheNumber,TAGNUMBER3,4);
+            break;
+        }
+        case 4:
+        {
+            memcpy(cPartitionTheNumber,TAGNUMBER4,4);
+            break;
+        }
+        case 5:
+        {
+            memcpy(cPartitionTheNumber,TAGNUMBER5,4);
+            break;
+        }
+        default:
+        {
+            memcpy(cPartitionTheNumber,TAGNUMBER1,4);
+        }
+    }
+#ifdef SCIA_LIB
+    if(SerialChannelPtr==&SciaRegs)
+    {
+        return SciaUartSend(cPartitionTheNumber, 8);
+    }
+#endif
+#ifdef SCIB_LIB
+    if(SerialChannelPtr==&ScibRegs)
+    {
+        //return ScibUartSend(cPartitionTheNumber, 8);
+        ScibUartSend(cPartitionTheNumber, 8);
+        return 0;
+    }
+#endif
+#ifdef SCIC_LIB
+    if(SerialChannelPtr==&ScicRegs)
+    {
+        return ScicUartSend(cPartitionTheNumber, 8);
+    }
+#endif
+#ifdef SCID_LIB
+    if(SerialChannelPtr==&ScidRegs)
+    {
+        return ScidUartSend(cPartitionTheNumber, 8);
+    }
+#endif
+
+    return -1;
 }
 
-void SciSendInteger(long int *iVariableToSend)
+int SciSendUnsignedInteger(unsigned long int *uiVariableToSend,volatile struct SCI_REGS* SerialChannelPtr)
 {
     char cPartitionTheNumber[4];
-    cPartitionTheNumber[3] = __byte((int *)iVariableToSend,3);
-    cPartitionTheNumber[2] = __byte((int *)iVariableToSend,2);
-    cPartitionTheNumber[1] = __byte((int *)iVariableToSend,1);
-    cPartitionTheNumber[0] = __byte((int *)iVariableToSend,0);
-    ScibUartSend(cPartitionTheNumber, 4);
-}
-void SciSendUnsignedInteger(unsigned long int *uiVariableToSend)
-{
-    //char cPartitionTheNumber[4];
-    cSendUnsignedInteger[3] = __byte((int *)uiVariableToSend,3);
-    cSendUnsignedInteger[2] = __byte((int *)uiVariableToSend,2);
-    cSendUnsignedInteger[1] = __byte((int *)uiVariableToSend,1);
-    cSendUnsignedInteger[0] = __byte((int *)uiVariableToSend,0);
-    ScibUartSend(cSendUnsignedInteger, 4);
+    cPartitionTheNumber[3] = __byte((int *)uiVariableToSend,3);
+    cPartitionTheNumber[2] = __byte((int *)uiVariableToSend,2);
+    cPartitionTheNumber[1] = __byte((int *)uiVariableToSend,1);
+    cPartitionTheNumber[0] = __byte((int *)uiVariableToSend,0);
+    if(SerialChannelPtr==&ScibRegs)
+    {
+        ScibUartSend(cPartitionTheNumber, 4);
+        return 0;
+    }
+
+    return 0;
 }

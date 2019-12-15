fd7c1074186fed1b2927efce5ed6259e4d0056da
hakan saraç <hakansrc@msn.com>
2019-12-15 18:45:26 +0300

    matlab seri data okuma kısmına güncellemeler yapıldı ve kullanmaya hazır hale getirildi. Aynı zamanda raw data işlemek için RawDataHandler.m yazıldı. Şimdilik forloopları halinde işlediği için çok verimsiz bir kod. MATLAB vectorleriyle işlem gerçekleştirmek için RawDataHandlerPro.m üzerinde çalışılıyor. RawDataHandlerPro.m henüz prototip aşamasında.

--- Garbage/serial/SaveTheSerialData.m
+++ Garbage/serial/SaveTheSerialData.m
@@ -1,50 +1,55 @@
 clc
 clear
 % fclose(instrfind) % call this command for stopping callback function
+%% IMPORTANT VARIABLES
+% global variables are used for communication between the main function and
+% the callback function
+global SerialReadArray                      %the read values are put here first, in bytes
+global ShowTheDataFlag                      %when this is set to 1, the main function shows the WindowArea of received data to the user, used for live visualizing of the main signal
+global RawDataIndex RawDataIndexPrevious    %To track down the index of the raw data
+global TheSaveArrayRaw                      %This is the raw data array, inside which the all received data is put. This array is saved along with the tags when the received number of bytes reaches TheSaveUpperLimit value
+global GoToGarbage                          %During the saving of TheSaveArrayRaw, a little bit of data goes to garbage
+global CallbackFunctionByteNumber           %This value states when to call the callback function
+global TheSaveUpperLimit                    %This value is the number of bytes of data to be saved as raw data (TheSaveArrayRaw)
+global WindowArea                           %This variable states amount of data to be shown to the user during the operation
+global SaveTheDataFlag                      %This is just a flag, for non user operations
 %% IMPORTANT DEFINITIONS
-global CallbackFunctionByteNumber
-global TheSaveAmount
 BaudRateValue = 460800*2;               %921600 is the maximum baudrate value for now
 TimeoutValue = 6;                       %Allowed time in seconds to complete read and write operations, returned as a numeric value.
 CallbackFunctionByteNumber=512;         %the callback function is called when this amount of bytes are read from the channel
-TheSaveAmount = 100*1024;                %the callback function save array size in bytes
-TagNumber1 = '0001';
+TheSaveUpperLimit = 0.5*1024*1024;       
+WindowArea = 50*1024;
+TagNumber1 = '0001';                    %Tag values, these values should match with the tags stated in the DSP code (tag values are stored along with the TheSaveArrayRaw)
 TagNumber2 = '0002';
 TagNumber3 = '0003';
 TagNumber4 = '0004';
 TagNumber5 = '0005';
 
-EnableSaving = 0;                         %set 0 in order to disable saving of workspace
-EnablePlotting = 1;                     %set 0 in order to disable plotting of taken variables
-
-%% IMPORTANT VARIABLES
-% global variables (used for getting variables from the callback function)
-global SerialReadArray          %the read values are put here first, in bytes
-global SerialReadOrdered        %the read values at SerialReadArray are aligned inside this array, in bytes
-global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it, 1 or 0
-global NumberOfReceivedBytes    %this is a variable that stores number of bytes read from the serial channel, required for array operations,
-global TheSaveArrayRaw             %The callback function stores the data inside this large array, the values are converted from this array
+EnableSaving = 1;                       %set 0 in order to disable saving of variable (recommended to stay at 1)
+EnablePlotting = 1;                     %set 0 in order to disable plotting of taken variables (for live visualization)
 
 %% Main function
-NumberOfReceivedBytes = 0; %init the number of bytes
-SaveTheArrayFlag = 0;       %init the flag
-SerialChannel = serial('COM4','BaudRate',BaudRateValue,'Parity','none','Timeout',6);
-SerialChannel.BytesAvailableFcnCount = CallbackFunctionByteNumber;
-SerialChannel.BytesAvailableFcnMode = 'byte';
-SerialChannel.BytesAvailableFcn = @SerialReadCallbackFunction;
-fopen(SerialChannel);
-TheSaveArrayRaw = zeros(TheSaveAmount,1);          %init the array
+RawDataIndex = 0;                               %initialize the variable
+RawDataIndexPrevious = 0;                       %initialize the variable
+ShowTheDataFlag = 0;                            %initialize the variable
+SaveTheDataFlag = 0;                            %initialize the variable
+TheSaveArrayRaw = zeros(TheSaveUpperLimit,1);	%initialize the variable
+MainSignalOffset = 0;                           %initialize the variable
+IsMainSignalOffsetProper = 0;                   %initialize the variable
+
+SerialChannel = serial('COM4','BaudRate',BaudRateValue,'Parity','none','Timeout',6);    %open the serial channel
+SerialChannel.BytesAvailableFcnCount = CallbackFunctionByteNumber;  %set the callback function byte number
+SerialChannel.BytesAvailableFcnMode = 'byte';                       %set the callback function type
+SerialChannel.BytesAvailableFcn = @SerialReadCallbackFunction;      %state the callback function to be called
+fopen(SerialChannel);                                               %open the serial channel port
 if (EnableSaving==1)
-    mkdir TestData                                  %create directory for saving data
+    mkdir TestData                              %create directory for saving data
 end
-MainSignalOffset = 0;
-IsMainSignalOffsetProper = 0;
 while(1)
-    
-    if(SaveTheArrayFlag==1)
+    if(ShowTheDataFlag==1)
         IsMainSignalOffsetProper = 0;
-        TheSaveArrayScreened = TheSaveArrayRaw;
-        %% Tag 1 conversion and elimination
+        TheSaveArrayScreened = TheSaveArrayRaw(RawDataIndexPrevious:(RawDataIndexPrevious+WindowArea),1);
+        %% Tag 1 conversion and elimination for live visualization
         Tag_1_Indices = strfind(TheSaveArrayScreened',TagNumber1);
         if(isempty(Tag_1_Indices)==0)
             if(IsMainSignalOffsetProper==0)
@@ -66,7 +71,7 @@ while(1)
                 end
             end
         end
-        %% Tag 2 conversion and elimination
+        %% Tag 2 conversion and elimination for live visualization
         Tag_2_Indices = strfind(TheSaveArrayScreened',TagNumber2);
         if(isempty(Tag_2_Indices)==0)
             if(IsMainSignalOffsetProper==0)
@@ -88,7 +93,7 @@ while(1)
                 end
             end
         end
-        %% Tag 3 conversion and elimination
+        %% Tag 3 conversion and elimination for live visualization
         Tag_3_Indices = strfind(TheSaveArrayScreened',TagNumber3);
         if(isempty(Tag_3_Indices)==0)
             if(IsMainSignalOffsetProper==0)
@@ -110,7 +115,7 @@ while(1)
                 end
             end
         end      
-        %% Tag 4 conversion and elimination
+        %% Tag 4 conversion and elimination for live visualization
         Tag_4_Indices = strfind(TheSaveArrayScreened',TagNumber4);
         if(isempty(Tag_4_Indices)==0)
             if(IsMainSignalOffsetProper==0)
@@ -132,7 +137,7 @@ while(1)
                 end
             end
         end  
-        %% Tag 5 conversion and elimination
+        %% Tag 5 conversion and elimination for live visualization
         Tag_5_Indices = strfind(TheSaveArrayScreened',TagNumber5);
         if(isempty(Tag_5_Indices)==0)
             if(IsMainSignalOffsetProper==0)
@@ -154,22 +159,30 @@ while(1)
                 end
             end
         end          
-        %% Raw data conversion
-        MainSignalConverted = zeros(TheSaveAmount/4-MainSignalOffset-8*numel(Tag_1_Indices)-8*numel(Tag_2_Indices)-8*numel(Tag_3_Indices)-8*numel(Tag_4_Indices)-8*numel(Tag_5_Indices),1); %init the array
-        for i=1:1:(TheSaveAmount/4-MainSignalOffset-8*numel(Tag_1_Indices)-8*numel(Tag_2_Indices)-8*numel(Tag_3_Indices)-8*numel(Tag_4_Indices)-8*numel(Tag_5_Indices))
+        %% Raw data conversion for live visualization
+        MainSignalConverted = zeros(WindowArea/4-MainSignalOffset-8*numel(Tag_1_Indices)-8*numel(Tag_2_Indices)-8*numel(Tag_3_Indices)-8*numel(Tag_4_Indices)-8*numel(Tag_5_Indices),1); %init the array
+        for i=1:1:(WindowArea/4-MainSignalOffset-8*numel(Tag_1_Indices)-8*numel(Tag_2_Indices)-8*numel(Tag_3_Indices)-8*numel(Tag_4_Indices)-8*numel(Tag_5_Indices))
             MainSignalConverted(i) = typecast(uint32(hex2dec(strcat(dec2hex(TheSaveArrayScreened(4+4*(i-1)+MainSignalOffset),2),dec2hex(TheSaveArrayScreened(3+4*(i-1)+MainSignalOffset),2),dec2hex(TheSaveArrayScreened(2+4*(i-1)+MainSignalOffset),2),dec2hex(TheSaveArrayScreened(1+4*(i-1)+MainSignalOffset),2)))),'single');
         end
-              
-        if (EnablePlotting==1)
+        %%      
+        if (EnablePlotting==1)  %for live visualization
             plot(MainSignalConverted);
             ylim([0 1])
             drawnow;
         end
         if (EnableSaving==1)
-            FileName = sprintf("TestData/TestData%s", datestr(now, 'ddmmyyHHMMSS'));
-            save(FileName);
+            if(SaveTheDataFlag==1)
+                FileName = sprintf("TestData/TestData%s", datestr(now, 'ddmmyyHHMMSS'));
+                save(FileName,'TheSaveArrayRaw','TagNumber1','TagNumber2','TagNumber3','TagNumber4','TagNumber5');
+                TheSaveArrayRaw = zeros(TheSaveUpperLimit,1);          %clear the raw data because it is saved
+                RawDataIndexPrevious = 0;
+                RawDataIndex = 0;
+                SaveTheDataFlag = 0;                                    % let the callback function continue
+            end
+        else
+            SaveTheDataFlag = 0;        % just pass
         end
-        SaveTheArrayFlag = 0;
+        ShowTheDataFlag = 0;
     else
         if(EnableSaving==1)
             pause(1);%wait for one second between operations
@@ -181,25 +194,41 @@ while(1)
 end
 %% The callback function
 function [] =  SerialReadCallbackFunction(TheSerialChannel,TheEvent)
-global NumberOfReceivedBytes
+global RawDataIndex RawDataIndexPrevious
 global CallbackFunctionByteNumber
 global SerialReadArray          %the read values are put here first
-global TheSaveArrayRaw             %The callback function stores the data inside this large array, initialized inside the main function
-global SaveTheArrayFlag         %when this is set to 1, the main function takes data from the array and saves it
-global TheSaveAmount
+global TheSaveArrayRaw          %The callback function stores the data inside this large array, initialized inside the main function
+global ShowTheDataFlag          %when this is set to 1, the main function takes data from TheSaveArrayRaw and show the last WindowArea amount of it to the user
+global WindowArea               %This variable states amount of data to be shown to the user during the operation
+global GoToGarbage              %During the saving of TheSaveArrayRaw, a little bit of data goes to garbage
+global SaveTheDataFlag          %This is just a flag, for non user operations
+global TheSaveUpperLimit        %This value is the number of bytes of data to be saved as raw data (TheSaveArrayRaw)
 
-if(SaveTheArrayFlag==0)
+if(SaveTheDataFlag == 0)
+    % fill in the raw data array
     SerialReadArray = fread(TheSerialChannel,TheSerialChannel.BytesAvailable);
-    TheSaveArrayRaw((NumberOfReceivedBytes+1):(NumberOfReceivedBytes+CallbackFunctionByteNumber),1)=SerialReadArray;
-    NumberOfReceivedBytes = NumberOfReceivedBytes+512;
+    TheSaveArrayRaw((RawDataIndex+1):(RawDataIndex+CallbackFunctionByteNumber),1)=SerialReadArray;
+    RawDataIndex = RawDataIndex+512;
 else
-    GoToGarbage = fread(TheSerialChannel,TheSerialChannel.BytesAvailable); %just read and pass, dont store , otherwise the callback does not work
+    %it means we are saving right now, dont mess with the raw data array for a
+    %little while, be a patient child.
+    GoToGarbage = fread(TheSerialChannel,TheSerialChannel.BytesAvailable); %just read and pass, dont store , otherwise the callback does not work  
+end
+if(ShowTheDataFlag == 0)
+    % if requested amount of data have arrived, show them to us during the
+    % operation
+    if((RawDataIndex-RawDataIndexPrevious)>WindowArea)
+        RawDataIndexPrevious = RawDataIndex-WindowArea;
+        ShowTheDataFlag = 1;
+    end
+    
+end
+if(RawDataIndex >= TheSaveUpperLimit)
+    % Raw data has reached to its specified limit, just save it and wait
+    % for new data when the saving operation has finished
+    SaveTheDataFlag = 1;
 end
 
-if(NumberOfReceivedBytes>=TheSaveAmount)
-    
-    NumberOfReceivedBytes = 0;
-    SaveTheArrayFlag = 1;
-end
+
 
 end

7a17a15faf0da2c45dae07bfd482da02d9af922e
hakan saraç <hakansrc@msn.com>
2020-08-21 22:35:14 +0300

    torque ripple prediction have been integrated into one module simulation. First operations seem successful. gonna be investigation more.

--- Simulation/MPC/mpc_model_parameters.m
+++ Simulation/MPC/mpc_model_parameters.m
@@ -3,7 +3,7 @@ clc
 %% Simulation parameters
 Sampling_time = 1e-6;
 ref_frequency = 100*pi;
-Tfinal = 3; %seconds
+Tfinal = 2 ; %seconds
 %% PMSM parameters
 MotorSpeed = 600; % rpm
 PolePairs = 10;
@@ -85,12 +85,12 @@ iqrated = Trated/(1.5*PolePairs*FluxPM);
 %% Other parameters
 Load_Nominal_Freq = 50;
 THD_mean_frequency = 50;
-SpeedRef1 = 300; % RPM
-SpeedRef2 = 300;
-SpeedRefUpdateTime = 2;
+SpeedRef1 = 100; % RPM
+SpeedRef2 = 400;
+SpeedRefUpdateTime = 1.5;
 Tload1 = 10; %Nm
 Tload2 = Trated;
-TloadUpdateTime = 1;
+TloadUpdateTime = 0.5;
 FaultTime = 5;
 PmaxPerModule = 2000;
 RPMmaxPerModule = 600;

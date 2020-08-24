096236f99e6e740636ec38d61ace322d99e29c1c
hakan saraç <hakansrc@msn.com>
2020-08-24 15:48:09 +0300

    torque shared mpc results are added. they will also be gathered some time later.

--- Simulation/MPC/mpc_model_plot_dual_module.m
+++ Simulation/MPC/mpc_model_plot_dual_module.m
@@ -113,6 +113,7 @@ plot(out.Simout_TorqueSpeedMeas.M1_Te,'LineWidth',2);
 hold on
 plot(out.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
 grid on
+xlim([0.95 1.05])
 % ylim([-5 35])
 set(gca,'FontSize',FontSize,'FontWeight','bold')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
@@ -128,7 +129,7 @@ xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Torque (Nm)','FontSize',FontSize,'FontWeight','bold');
 legend('M1 Te + M2 Te','Tload')
 % ylim([-150 150])
-% ylim([-5 35])
+xlim([0.95 1.05])
 grid on
 
 
@@ -190,4 +191,46 @@ title('iq ripple')
 legend('M1','M2')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
 
+grid on
+%%
+figure
+subplot(3,1,1)
+hold all
+plot(out.Simout_PhaseCurrents_dq.M1_ids,'LineWidth',2);
+plot(out.Simout_PhaseCurrents_dq.M1_iqs,'LineWidth',2);
+hold off
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+% title('M1 Idq')
+legend('M1 Id','M1 Iq')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+xlim([2.5 2.55])
+grid on
+
+% figure
+subplot(3,1,2)
+hold all
+plot(out.Simout_PhaseCurrents_dq.M2_ids,'LineWidth',2);
+plot(out.Simout_PhaseCurrents_dq.M2_iqs,'LineWidth',2);
+hold off
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+% title('M1 Idq')
+legend('M2 Id','M2 Iq')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+xlim([2.5 2.55])
+grid on
+
+% figure
+subplot(3,1,3)
+hold all
+plot(out.Simout_Fsw.M1_fsw_out/1000, 'LineWidth',2);
+plot(out.Simout_Fsw.M2_fsw_out/1000, 'LineWidth',2);
+hold off
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Fsw (kHz)','FontSize',FontSize,'FontWeight','bold');
+title('Switching frequencies')
+legend('Module 1','Module 2')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+xlim([2.5 2.55])
 grid on

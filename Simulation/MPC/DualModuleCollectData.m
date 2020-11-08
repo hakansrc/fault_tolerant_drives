921e6d5ddbfce360e34d5237f32ff5767636a435
hakan saraç <hakansrc@msn.com>
2020-11-08 15:21:49 +0300

    More additions on simulation side. It is now easier to collect data for different control methods and compare their results with each other.

--- Simulation/MPC/DualModuleCollectData.m
+++ Simulation/MPC/DualModuleCollectData.m
@@ -1,16 +1,27 @@
 mpc_model_parameters_dual_module
 
 for RefModeSelection = 1:3
+    tic
+    RefModeSelection
     sim('mpc_model_dual_module.slx');
     if(RefModeSelection == 1)
         savename = strcat('SimulationResults/Cost_Optimal');
+        assignin('base','Results_Cost_Optimal',ans);     
+        save(savename,'Results_Cost_Optimal');
+        clear('Results_Cost_Optimal');
     end
     if(RefModeSelection == 2)
         savename = strcat('SimulationResults/Proportional_Optimal');
+        assignin('base','Results_Proportional_Optimal',ans);
+        save(savename,'Results_Proportional_Optimal');      
+        clear('Results_Cost_Optimal');        
     end    
     if(RefModeSelection == 3)
         savename = strcat('SimulationResults/Equal_Share');
-    end       
-    save(savename,'ans');
+        assignin('base','Results_Equal_Share',ans);
+        save(savename,'Results_Equal_Share');                
+        clear('Results_Cost_Optimal');       
+    end
+    toc
 end
 %%
--- Simulation/MPC/DualModulePlotData.m
+++ Simulation/MPC/DualModulePlotData.m
@@ -1,204 +1,315 @@
 clear
 clc
-load('Cost_Optimal.mat');
+load('SimulationResults/Cost_Optimal.mat');
+load('SimulationResults/Equal_Share.mat');
+load('SimulationResults/Proportional_Optimal.mat');
 
+
+%% Speed Performance Comparison
+FontSize = 16;
+SpeedPlot_xlim = [0 3];
+% SpeedPlot_ylim = [];
+figure
+plot(Results_Cost_Optimal.Simout_TorqueSpeedMeas.M1_wmech*60/(2*pi),'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_TorqueSpeedMeas.M1_wmech*60/(2*pi),'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_TorqueSpeedMeas.M1_wmech*60/(2*pi),'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_TorqueSpeedRef.wref*60/(2*pi),'LineWidth',2);
+hold off
+grid on
+title('Speed Performance of Different Methods')
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Shaft Speed (RPM)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share','Speed Ref','Location','southeast')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+xlim(SpeedPlot_xlim);
+%% Idq comparison 
 FontSize = 16;
 figure
-refs_xlim = [0 2];
-refs_ylim = [-10 16];
-subplot(2,2,1)
-plot(ans.Simout_Reference_Idq.M1_iqref,'LineWidth',2);
+IdqPlot_xlim = [2.5 2.6];
+
+subplot(2,2,1) % M1 Id's are here
+
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_dq.M1_ids,'LineWidth',2);
 hold on
-plot(ans.Simout_PhaseCurrents_dq.M1_iqs,'LineWidth',2);
-legend('M1 iqref','M1 iq')
-title('M1 iq (faulty)')
-xlim(refs_xlim)
-ylim(refs_ylim)
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_dq.M1_ids,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_dq.M1_ids,'LineWidth',2);
 grid on
+title('M1 Id Waveforms for Different Methods')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,2)
-plot(ans.Simout_Reference_Idq.M1_idref,'LineWidth',2);
+xlim(IdqPlot_xlim);
+
+subplot(2,2,2) % M2 Id's are here
+
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_dq.M2_ids,'LineWidth',2);
 hold on
-plot(ans.Simout_PhaseCurrents_dq.M1_ids,'LineWidth',2);
-legend('M1 idref','M1 id')
-title('M1 id (faulty)')
-xlim(refs_xlim)
-ylim(refs_ylim)
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_dq.M2_ids,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_dq.M2_ids,'LineWidth',2);
+
 grid on
+title('M2 Id Waveforms for Different Methods')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,3)
-plot(ans.Simout_Reference_Idq.M2_iqref,'LineWidth',2);
+xlim(IdqPlot_xlim);
+
+subplot(2,2,3) % M1 Iq's are here
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_dq.M1_iqs,'LineWidth',2);
 hold on
-plot(ans.Simout_PhaseCurrents_dq.M2_iqs,'LineWidth',2);
-legend('M2 iqref','M2 iq')
-title('M2 iq (healthy)')
-xlim(refs_xlim)
-ylim(refs_ylim)
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_dq.M1_iqs,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_dq.M1_iqs,'LineWidth',2);
 grid on
+title('M1 Iq Waveforms for Different Methods')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,4)
-plot(ans.Simout_Reference_Idq.M2_idref,'LineWidth',2);
+xlim(IdqPlot_xlim);
+
+subplot(2,2,4) % M2 Iq's are here
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_dq.M2_iqs,'LineWidth',2);
 hold on
-plot(ans.Simout_PhaseCurrents_dq.M2_ids,'LineWidth',2);
-legend('M2 idref','M2 id')
-title('M2 id (healthy)')
-xlim(refs_xlim)
-ylim(refs_ylim)
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_dq.M2_iqs,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_dq.M2_iqs,'LineWidth',2);
 grid on
+title('M2 Iq Waveforms for Different Methods')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-%%
+xlim(IdqPlot_xlim);
+
+%% Torques comparison
 FontSize = 16;
 figure
-refs_xlim = [1.8 2];
-refs_ylim = [-10 16];
-subplot(2,2,1)
-plot(ans.Simout_Reference_Idq.M1_iqref,'LineWidth',2);
+TorquePlot_xlim = [0 3];%[1.99 2.1];
+TorquePlot_ylim = [-2 82];
+TorquePlot_ylim_sum = [35 100];
+
+
+subplot(3,1,1) 
+plot(Results_Cost_Optimal.Simout_TorqueSpeedMeas.M1_Te,'LineWidth',2);
 hold on
-plot(ans.Simout_PhaseCurrents_dq.M1_iqs,'LineWidth',2);
-legend('M1 iqref','M1 iq')
-title('M1 iq (faulty)')
-xlim(refs_xlim)
-ylim(refs_ylim)
-grid on
-xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
-set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,2)
-plot(ans.Simout_Reference_Idq.M1_idref,'LineWidth',2);
+plot(Results_Proportional_Optimal.Simout_TorqueSpeedMeas.M1_Te,'LineWidth',2);
 hold on
-plot(ans.Simout_PhaseCurrents_dq.M1_ids,'LineWidth',2);
-legend('M1 idref','M1 id')
-title('M1 id (faulty)')
-xlim(refs_xlim)
-ylim(refs_ylim)
+plot(Results_Equal_Share.Simout_TorqueSpeedMeas.M1_Te,'LineWidth',2);
 grid on
+xlim(TorquePlot_xlim)
+ylim(TorquePlot_ylim)
+title('M1 (Faulty) Torque Waveforms for Different Methods')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
-set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,3)
-plot(ans.Simout_Reference_Idq.M2_iqref,'LineWidth',2);
-hold on
-plot(ans.Simout_PhaseCurrents_dq.M2_iqs,'LineWidth',2);
-legend('M2 iqref','M2 iq')
-title('M2 iq (healthy)')
-xlim(refs_xlim)
-ylim(refs_ylim)
-grid on
-xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
-set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,4)
-plot(ans.Simout_Reference_Idq.M2_idref,'LineWidth',2);
-hold on
-plot(ans.Simout_PhaseCurrents_dq.M2_ids,'LineWidth',2);
-legend('M2 idref','M2 id')
-title('M2 id (healthy)')
-xlim(refs_xlim)
-ylim(refs_ylim)
-grid on
-xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Torque (Nm)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
 
-%%
-figure
-subplot(2,1,1)
-plot(ans.Simout_TorqueSpeedMeas.M1_Te,'LineWidth',2);
+
+subplot(3,1,2) 
+plot(Results_Cost_Optimal.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
 hold on
-plot(ans.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
+plot(Results_Proportional_Optimal.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
 grid on
-% ylim([0 15])
-set(gca,'FontSize',FontSize,'FontWeight','bold')
+xlim(TorquePlot_xlim)
+ylim(TorquePlot_ylim)
+title('M2 (Healthy) Torque Waveforms for Different Methods')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Torque (Nm)','FontSize',FontSize,'FontWeight','bold');
-legend('M1 Te','M2 Te')
-subplot(2,1,2)
-grid on 
-plot((ans.Simout_TorqueSpeedMeas.M1_Te+ans.Simout_TorqueSpeedMeas.M2_Te),'LineWidth',2);
-hold on
-plot(ans.Simout_TorqueSpeedRef.Combined_Tload,'LineWidth',2);
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
+
+subplot(3,1,3) 
+
+plot(Results_Cost_Optimal.Simout_TorqueSpeedMeas.M1_Te+Results_Cost_Optimal.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_TorqueSpeedMeas.M1_Te+Results_Proportional_Optimal.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_TorqueSpeedMeas.M1_Te+Results_Equal_Share.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
+grid on
+xlim(TorquePlot_xlim)
+ylim(TorquePlot_ylim_sum)
+title('M1+M2 Torque Waveforms for Different Methods')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Torque (Nm)','FontSize',FontSize,'FontWeight','bold');
-legend('M1 Te + M2 Te','Tload')
-grid on
-%%
-figure
-subplot(2,1,1)
-plot(ans.Simout_TorqueSpeedMeas.M1_Te,'LineWidth',2);
-hold on
-plot(ans.Simout_TorqueSpeedMeas.M2_Te,'LineWidth',2);
-grid on
-xlim([1.8 2])
-% ylim([0 15])
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Torque (Nm)','FontSize',FontSize,'FontWeight','bold');
-legend('M1 Te','M2 Te')
-subplot(2,1,2)
-grid on 
-plot((ans.Simout_TorqueSpeedMeas.M1_Te+ans.Simout_TorqueSpeedMeas.M2_Te),'LineWidth',2);
-hold on
-plot(ans.Simout_TorqueSpeedRef.Combined_Tload,'LineWidth',2);
-xlim([1.8 2])
-set(gca,'FontSize',FontSize,'FontWeight','bold')
-xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Torque (Nm)','FontSize',FontSize,'FontWeight','bold');
-legend('M1 Te + M2 Te','Tload')
-grid on
-%%
-% close all
+
+%% Overall Losses
 FontSize = 16;
 figure
-loss_xlim = [1.8 2];
-% hold all
-subplot(2,2,1)
-plot(ans.Simout_losses.M1_loss,'LineWidth',2);
-title('M1 loss')
-xlim(loss_xlim)
-ylim([0 120])
+Total_Losses_xlim = [0 3];%[1.99 2.1];
+Total_Losses_ylim = [0 600];
+TorquePlot_ylim_sum = [35 100];
+subplot(2,1,1)
+plot(Results_Cost_Optimal.Simout_losses.Mean_loss,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_losses.Mean_loss,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_losses.Mean_loss,'LineWidth',2);
+hold off
 grid on
+title('Total losses (Moving Average)')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Loss (W)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,2)
-plot(ans.Simout_losses.M2_loss,'LineWidth',2);
-title('M2 loss')
-xlim(loss_xlim)
-ylim([0 120])
+xlim(Total_Losses_xlim)
+ylim(Total_Losses_ylim)
+subplot(2,1,2)
+plot(Results_Cost_Optimal.Simout_losses.Total_loss,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_losses.Total_loss,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_losses.Total_loss,'LineWidth',2);
+hold off
 grid on
+title('Total losses')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
 ylabel('Loss (W)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,3)
-plot(ans.Simout_losses.Total_loss,'LineWidth',2);
-title('Total loss')
-xlim(loss_xlim)
-ylim([0 120])
+xlim(Total_Losses_xlim)
+ylim(Total_Losses_ylim)
+%% Conduction Losses
+FontSize = 16;
+figure
+Core_Losses_xlim = [1.8 1.9];
+Core_Losses_ylim = [100 200];
+Cond_Losses_xlim = Core_Losses_xlim;%[1.99 2.1];
+Cond_Losses_ylim = [0 130];
+TorquePlot_ylim_sum = [35 100];
+subplot(2,1,1)
+plot(Results_Cost_Optimal.Simout_losses.Total_cond_loss,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_losses.Total_cond_loss,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_losses.Total_cond_loss,'LineWidth',2);
+hold off
 grid on
+title('Conduction losses')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Loss (W)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Conduction Loss (W)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-subplot(2,2,4)
-plot(ans.Simout_losses.Mean_loss,'LineWidth',2);
-title('Mean loss')
-xlim(loss_xlim)
-ylim([0 120])
+xlim(Cond_Losses_xlim)
+ylim(Cond_Losses_ylim)
+subplot(2,1,2)
+plot(Results_Cost_Optimal.Simout_losses.Total_core_loss,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_losses.Total_core_loss,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_losses.Total_core_loss,'LineWidth',2);
+hold off
 grid on
+title('Core losses')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Loss (W)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Core Loss (W)','FontSize',FontSize,'FontWeight','bold');
+legend('Cost Optimal','Proportional Optimal','Equal Share')
 set(gca,'FontSize',FontSize,'FontWeight','bold')
-% hold off
+xlim(Core_Losses_xlim)
+ylim(Core_Losses_ylim)
+%% Phase currents
+FontSize = 16;
+figure
+PhaseCurrents_xlim = [1.8 1.9];
+% PhaseCurrents_ylim = [100 200];
+subplot(3,2,1)
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_abc.M1_Ia,'LineWidth',2);
+hold on
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_abc.M1_Ib,'LineWidth',2);
+hold on
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_abc.M1_Ic,'LineWidth',2);
+hold off
+grid on
+xlim(PhaseCurrents_xlim)
+% ylim(PhaseCurrents_ylim)
+title('M1 Phase current Waveforms for Cost Optimal')
 xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
-ylabel('Loss (W)','FontSize',FontSize,'FontWeight','bold');
-% title('Iabc M1')
-% legend('M1_loss','M2_loss','Total_loss','Mean_loss')
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Ia','Ib','Ic')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+
+subplot(3,2,2)
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_abc.M2_Ia,'LineWidth',2);
+hold on
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_abc.M2_Ib,'LineWidth',2);
+hold on
+plot(Results_Cost_Optimal.Simout_PhaseCurrents_abc.M2_Ic,'LineWidth',2);
+hold off
+grid on
+xlim(PhaseCurrents_xlim)
+title('M1 Phase current Waveforms for Cost Optimal')
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Ia','Ib','Ic')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+
+subplot(3,2,3)
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_abc.M1_Ia,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_abc.M1_Ib,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_abc.M1_Ic,'LineWidth',2);
+hold off
+grid on
+xlim(PhaseCurrents_xlim)
+title('M1 Phase current Waveforms for Proportional Optimal')
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Ia','Ib','Ic')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+
+subplot(3,2,4)
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_abc.M2_Ia,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_abc.M2_Ib,'LineWidth',2);
+hold on
+plot(Results_Proportional_Optimal.Simout_PhaseCurrents_abc.M2_Ic,'LineWidth',2);
+hold off
+grid on
+xlim(PhaseCurrents_xlim)
+title('M2 Phase current Waveforms for Proportional Optimal')
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Ia','Ib','Ic')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+
+subplot(3,2,5)
+plot(Results_Equal_Share.Simout_PhaseCurrents_abc.M1_Ia,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_abc.M1_Ib,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_abc.M1_Ic,'LineWidth',2);
+hold off
+grid on
+xlim(PhaseCurrents_xlim)
+title('M1 Phase current Waveforms for Results Equal Share')
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Ia','Ib','Ic')
+set(gca,'FontSize',FontSize,'FontWeight','bold')
+
+subplot(3,2,6)
+plot(Results_Equal_Share.Simout_PhaseCurrents_abc.M2_Ia,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_abc.M2_Ib,'LineWidth',2);
+hold on
+plot(Results_Equal_Share.Simout_PhaseCurrents_abc.M2_Ic,'LineWidth',2);
+hold off
+grid on
+xlim(PhaseCurrents_xlim)
+title('M2 Phase current Waveforms for Results Equal Share')
+xlabel('Time (sec)','FontSize',FontSize,'FontWeight','bold');
+ylabel('Current (A)','FontSize',FontSize,'FontWeight','bold');
+legend('Ia','Ib','Ic')
 set(gca,'FontSize',FontSize,'FontWeight','bold')

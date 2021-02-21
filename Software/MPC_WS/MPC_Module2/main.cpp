fc3f0031a6279aff06e183a54be445526a30bb8a
hakan saraç <hakansrc@msn.com>
2021-02-21 13:50:56 +0300

    added tag for one module stable operating version

--- Software/MPC_WS/MPC_Module2/main.cpp
+++ Software/MPC_WS/MPC_Module2/main.cpp
@@ -5,8 +5,6 @@
 __interrupt void cpu_timer0_isr(void);  /*prototype of the ISR functions*/
 __interrupt void cpu_timer1_isr(void);  /*prototype of the ISR functions*/
 __interrupt void cpu_timer2_isr(void);  /*prototype of the ISR functions*/
-__interrupt void epwm1_isr(void);       /*prototype of the ISR functions*/
-void InitializeEpwm1Registers();        /*prototype of epwm1 initialization function*/
 
 /**
  * main.c
@@ -40,15 +38,12 @@ int main(void)
     CpuTimer1Regs.TCR.all = 0x4000; // enable cpu timer interrupt
     CpuTimer2Regs.TCR.all = 0x4000; // enable cpu timer interrupt
 
-    InitializeEpwm1Registers();
-
-
     EALLOW;
     PieVectTable.TIMER0_INT = &cpu_timer0_isr;  /*specify the interrupt service routine (ISR) address to the PIE table*/
     PieVectTable.TIMER1_INT = &cpu_timer1_isr;  /*specify the interrupt service routine (ISR) address to the PIE table*/
     PieVectTable.TIMER2_INT = &cpu_timer2_isr;  /*specify the interrupt service routine (ISR) address to the PIE table*/
-    PieVectTable.EPWM1_INT = &epwm1_isr;        /*specify the interrupt service routine (ISR) address to the PIE table*/
     EDIS;
+
     IER |= M_INT1;  /*Enable the PIE group of Cpu timer 0 interrupt*/
     IER |= M_INT3;  /*Enable the PIE group of Epwm1 interrupt*/
     IER |= M_INT13; /*Enable the PIE group of Cpu timer 1 interrupt*/
@@ -71,7 +66,6 @@ int main(void)
     {
 
     }
-	return 0;
 }
 __interrupt void cpu_timer0_isr(void)
 {
@@ -90,48 +84,4 @@ __interrupt void cpu_timer2_isr(void)
     CpuTimer2.InterruptCount++;
 
 }
-void InitializeEpwm1Registers()
-{
-    EPwm1Regs.TBCTL.all = 0x00;
-    EPwm1Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;  // Set counter to be up-down
 
-    /*These bits select the time base clock pre-scale value (TBCLK = EPWMCLK/(HSPCLKDIV * CLKDIV):*/
-    EPwm1Regs.TBCTL.bit.CLKDIV = TB_DIV1;
-    EPwm1Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1;
-
-
-    EPwm1Regs.TBPRD = (200000000) / (10000) /2;  /*period is set to be 10khz (for up down count)*/
-
-    EPwm1Regs.CMPCTL.all = 0x00;
-    EPwm1Regs.CMPCTL.bit.SHDWAMODE = 1;         //only active registers are used
-
-    EPwm1Regs.AQCTLA.all = 0x00;
-    EPwm1Regs.AQCTLA.bit.CAU = AQ_SET;      //set high
-    EPwm1Regs.AQCTLA.bit.CAD = AQ_CLEAR;    //set low
-
-    EPwm1Regs.CMPA.bit.CMPA = EPwm1Regs.TBPRD / 2;    // Set compare A value
-
-    EPwm1Regs.ETSEL.all = 0x00;
-    EPwm1Regs.ETSEL.bit.INTSEL = 1; // When TBCTR == 0
-    EPwm1Regs.ETSEL.bit.INTEN = 1;  // Enable INT
-    EPwm1Regs.ETPS.all = 0x00;
-    EPwm1Regs.ETPS.bit.INTPRD = 1;  // Generate INT on first event
-
-    EALLOW;
-    GpioCtrlRegs.GPAPUD.bit.GPIO0 = 1;    // Disable pull-up on GPIO0 (EPWM1A)
-    GpioCtrlRegs.GPAPUD.bit.GPIO1 = 1;    // Disable pull-up on GPIO1 (EPWM1B)
-    GpioCtrlRegs.GPAGMUX1.bit.GPIO0 = 0;  // Configure GPIO0 as EPWM1A
-    GpioCtrlRegs.GPAGMUX1.bit.GPIO1 = 0;  // Configure GPIO1 as EPWM1B
-    GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 1;   // Configure GPIO0 as EPWM1A
-    GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 1;   // Configure GPIO1 as EPWM1B
-    EDIS;
-
-
-}
-__interrupt void epwm1_isr(void)
-{
-    Epwm1Counter++;
-    EPwm1Regs.ETCLR.bit.INT = 1;
-    // Acknowledge this interrupt to receive more interrupts from group 3
-    PieCtrlRegs.PIEACK.all = PIEACK_GROUP3;
-}

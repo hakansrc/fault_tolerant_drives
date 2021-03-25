04f02da63fa546420b5bc60c8b8ad1c324e3b705
hakan saraç <hakansrc@msn.com>
2021-03-25 15:04:31 +0300

    added sampling delay to single module operation

--- Simulation/MPC/mpc_model_parameters.m
+++ Simulation/MPC/mpc_model_parameters.m
@@ -3,7 +3,7 @@ clc
 %% Simulation parameters
 Sampling_time = 1e-6;
 ref_frequency = 100*pi;
-Tfinal = 3; %seconds
+Tfinal = 1.5; %seconds
 %% PMSM parameters
 MotorSpeed = 600; % rpm
 PolePairs = 10;
@@ -86,10 +86,10 @@ iqrated = Trated/(1.5*PolePairs*FluxPM);
 %% Other parameters
 Load_Nominal_Freq = 50;
 THD_mean_frequency = 50;
-SpeedRef1 = 50; % RPM
+SpeedRef1 = 80; % RPM
 SpeedRef2 = 80;
-SpeedRefUpdateTime = 1.5;
-Tload1 = 1.5; %Nm
+SpeedRefUpdateTime = 0.75;
+Tload1 = 20; %Nm
 Tload2 = 20;
 TloadUpdateTime = 0.75;
 FaultTime = 50;

475001c8cf1bc7c503da0b8de9e66aca4a34e91c
hakan saraç <hakansrc@msn.com>
2021-08-28 21:43:34 +0300

    switching frequency is boosted up to max during transients, depending on the speed error and speed change. Some cost function parameters are also updated.

---
+++ Simulation/MPC/datacompare.m
@@ -0,0 +1,67 @@
+clc
+clear
+close all
+
+load('fsw_constant.mat')
+load('fsw_variable.mat')
+
+%%
+close all
+
+figure(42)
+hold on
+plot(fsw_vrb.Simout_TorqueSpeedMeas.wmech)
+hold on
+plot(fsw_cns.Simout_TorqueSpeedMeas.wmech)
+legend('dynamic','normal')
+
+figure(43)
+hold on
+plot(fsw_vrb.Simout_PhaseCurrents_dq.iqs)
+hold on
+plot(fsw_cns.Simout_PhaseCurrents_dq.iqs)
+legend('dynamic','normal')
+
+figure(44)
+hold on
+plot(fsw_vrb.Simout_TorqueSpeedMeas.Te)
+hold on
+plot(fsw_cns.Simout_TorqueSpeedMeas.Te)
+legend('dynamic','normal')
+
+%%
+first = 'dynamic';
+second = 'static';
+figure(42)
+title('wmech')
+hold on
+plot(out.Simout_TorqueSpeedMeas.wmech)
+% hold on
+% plot(fsw_cns.Simout_TorqueSpeedMeas.wmech)
+% legend('dynamic','normal')
+legend(first,second)
+
+figure(43)
+title('iq current')
+hold on
+plot(out.Simout_PhaseCurrents_dq.iqs)
+% hold on
+% plot(fsw_cns.Simout_PhaseCurrents_dq.iqs)
+% legend('dynamic','normal')
+legend(first,second)
+
+figure(44)
+title('Te ')
+hold on
+plot(out.Simout_TorqueSpeedMeas.Te)
+% hold on
+% plot(fsw_cns.Simout_TorqueSpeedMeas.Te)
+% legend('dynamic','normal')
+legend(first,second)
+
+figure(45)
+title('iq error')
+hold on
+plot(out.Simout_PhaseCurrents_dq.iqs-out.Simout_PhaseCurrents_dq.iqref)
+legend(first,second)
+
